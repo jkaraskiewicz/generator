df731505ec505e5faac2735e5de92a7778ff97f0
Jacek Karaśkiewicz <jacek.karaskiewicz@gmail.com>
2017-05-13 19:34:05 +0200

    Refactored commands dispatcher

--- src/main/kotlin/commands/CommandsDispatcher.kt
+++ src/main/kotlin/commands/CommandsDispatcher.kt
@@ -2,34 +2,26 @@ package commands
 
 import filesystem.FileSystemGenerator
 import util.GeneratorConfig
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.nio.file.Paths
 
 class CommandsDispatcher(
         private val fileSystemGenerator: FileSystemGenerator,
         private val config: GeneratorConfig
 ) {
+    private val executor = CommandsExecutor(config)
 
     fun dispatchCommands() {
-        initializeGit()
         initializeGradleWrapper()
+        initializeGit()
     }
 
     private fun initializeGit() {
-        exec("git init")
+        executor.exec("git", "init")
         fileSystemGenerator.generateGitIgnores()
-        exec("git add -A .")
-        exec("git commit -m \"Initial commit\"")
+        executor.exec("git", "add", "-A", ".")
+        executor.exec("git", "commit", "-m", "Initial commit")
     }
 
     private fun initializeGradleWrapper() {
-        exec("gradle wrapper --distribution-type=${config.standardConfig.gradle.distribution}")
-    }
-
-    fun exec(command: String) {
-        val process = Runtime.getRuntime().exec(command, null, Paths.get(config.rootProjectDirPath).toFile())
-        val reader = BufferedReader(InputStreamReader(process.inputStream))
-        reader.lines().forEach { println(it) }
+        executor.exec("gradle wrapper --distribution-type=${config.standardConfig.gradle.distribution}")
     }
 }
---
+++ src/main/kotlin/commands/CommandsExecutor.kt
@@ -0,0 +1,29 @@
+package commands
+
+import util.GeneratorConfig
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import java.nio.file.Paths
+
+class CommandsExecutor(private val config: GeneratorConfig) {
+
+    fun exec(command: String) = launchProcess(createProcess(command, Paths.get(config.rootProjectDirPath).toFile()))
+
+    fun exec(vararg commands: String) = launchProcess(createProcess(commands, Paths.get(config.rootProjectDirPath).toFile()))
+
+    private fun createProcess(command: String, workingDir: File): Process {
+        return Runtime.getRuntime().exec(command, null, workingDir)
+    }
+
+    private fun createProcess(commands: Array<out String>, workingDir: File): Process {
+        return Runtime.getRuntime().exec(commands, null, workingDir)
+    }
+
+    private fun launchProcess(process: Process) {
+        process.apply { waitFor() }.let {
+            val reader = BufferedReader(InputStreamReader(it.inputStream))
+            reader.lines().forEach { println(it) }
+        }
+    }
+}
